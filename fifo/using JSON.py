ac5fa27a36783ea0b2eeb355b9cef8d55ccf988e
onionmccabbage <onionmccabbage@live.com>
2024-12-17 16:27:40 +0000

    working with JSON

--- fifo/using JSON.py
+++ fifo/using JSON.py
@@ -9,6 +9,11 @@ j = '''{
   "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
 }'''
 
+# we may convert a Python structure into a JSON string of text
+d = {'name':'Grommit', 'material':'plasticene', 'valid':True}
+g = json.dumps(d)
+
 # we may convert the JSON to a Python data structure
 s = json.loads(j) # this convetts the string of text into a Python dictionary
 print(s, type(s))
+print(g, type(g)) # a string
