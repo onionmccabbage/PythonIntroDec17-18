8e48c724c925d5a5c727ac3abbd6aba3093a15a9
onionmccabbage <onionmccabbage@live.com>
2024-12-17 10:53:56 +0000

    code blocks and funtions

--- using_data.py
+++ using_data.py
@@ -13,3 +13,21 @@ k = {5, 2, -99, True, 2, 'coffee', '2', '1', '4', 'coffee', 1}
 # be careful True evaluates to 1
 # also False evaluates to zero
 print(k, type(k))
+
+# code blocks. Begin with : then indent lines
+if True==1:
+    print('of course True equals 1')
+else:
+    print('ummmmm')
+# code block ends when the lines are no longer indented
+
+# another code block - here we define a function
+def fancyStuff():
+    a = 4
+    l = 3.2
+    result = l**a
+    return result
+# all the above lines are indented, so they belong to the code block (function)
+# we can invoke a function like this (i.e. call the function)
+r = fancyStuff()
+print(r)
