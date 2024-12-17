15267148eb927330ccc064200d5750846d6bea40
onionmccabbage <onionmccabbage@live.com>
2024-12-17 12:06:02 +0000

    using while

--- using_functions.py
+++ using_functions.py
@@ -5,11 +5,14 @@ import random # we can import from the Python standard library
 def askUser(): # here is a docstring
     '''we often write documentation for a function within tripple quotes
     This lets us us new-lines within the quoted text'''
-    r = input('What level random do we need: ') # this will be a string
-    # check we have a number
-    if r.isnumeric():
-        rand = random.randint(0, int(r))
-        return rand
+    while True: # this code block will run endlessly
+        r = input('What level random do we need: ') # this will be a string
+        # check we have a number
+        if r.isnumeric():
+            rand = random.randint(0, int(r))
+            return rand
+        else:
+            print(f'{r} is not a number')
     
 # exercise the code
 l = askUser()
