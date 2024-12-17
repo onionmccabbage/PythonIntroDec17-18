1433e8cf40ba7388d53ef7f3b79ea069598f1fc2
onionmccabbage <onionmccabbage@live.com>
2024-12-17 15:41:14 +0000

    writing new line character \n

--- fifo/file_output.py
+++ fifo/file_output.py
@@ -8,6 +8,7 @@ def printToFile(t):
     # anything to do with files must use a file access object
     fout = open('my_log.txt', 'at') # 'at' will append text to the file
     # we can use 'print' to send text to a file
+    # NB print will always add a new-line character after every printout
     print(f'{t} says Hello from Python', file=fout) # here we specify where the print should go
     fout.close() # good idea to tidy up when done
 
@@ -15,7 +16,8 @@ def writeTofile(t):
     '''Write text into a persistent file'''
     fout = open('file.txt', 'at') # we have a file access object
     with fout: # tidy way to handle file access object
-        fout.write(t)
+        # \n will write a new line character. \t will put a tab character
+        fout.write(f'{t}\n') # NB write does NOT auitomatically add a new line after output
     # the file access object will be closed autoamtically when the 'with' block ends
 
 
@@ -23,3 +25,4 @@ def writeTofile(t):
 r = random.randint(0,100)
 s = math.sqrt(r) # find the square root of a number
 printToFile(s) # here we call the function so it operates
+writeTofile(f'Text written to file: {r} and {s} together')
