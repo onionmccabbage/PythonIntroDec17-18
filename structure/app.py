df25e41e4c6031451d10ae93d36e6840e72f6632
onionmccabbage <onionmccabbage@live.com>
2024-12-17 13:39:35 +0000

    using import

--- structure/app.py
+++ structure/app.py
@@ -1,8 +1,15 @@
 # we may choose to import stuff from other modules
 import utility # we import the whole file
+from calc import addThem
+from calc import multThem
 
 d = input('enter something: ')
 
 r = utility.checkInt(d)
 
 print(r)
+
+# use the imported functions
+a = addThem(3,4)
+m = multThem(5,6)
+print(a,m)
