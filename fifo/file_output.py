88888cda03c3bd4bf1fc2a0e117f0d208dd09ed9
onionmccabbage <onionmccabbage@live.com>
2024-12-17 15:51:56 +0000

    writing try-except blocks

--- fifo/file_output.py
+++ fifo/file_output.py
@@ -6,11 +6,16 @@ import math # also from the Python standard library
 def printToFile(t):
     '''print some text to a persistent file'''
     # anything to do with files must use a file access object
-    fout = open('my_log.txt', 'at') # 'at' will append text to the file
-    # we can use 'print' to send text to a file
-    # NB print will always add a new-line character after every printout
-    print(f'{t} says Hello from Python', file=fout) # here we specify where the print should go
-    fout.close() # good idea to tidy up when done
+    try:
+        #  'at' will append text to the file
+        #  'xt' will try to acces the file for exclusive use (creates the file)
+        fout = open('my_log.txt', 'xt') # if the file exists this raises an exception
+        # we can use 'print' to send text to a file
+        # NB print will always add a new-line character after every printout
+        print(f'{t} says Hello from Python', file=fout) # here we specify where the print should go
+        fout.close() # good idea to tidy up when done
+    except FileExistsError as err:
+        print(f'There was a problem: {err}')
 
 def writeTofile(t):
     '''Write text into a persistent file'''
