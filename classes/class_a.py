053a89e3596f1612f06203ebc13684fe9ecd86f6
onionmccabbage <onionmccabbage@live.com>
2024-12-18 10:53:58 +0000

    a simple class

--- classes/class_a.py
+++ classes/class_a.py
@@ -10,6 +10,15 @@ n = [age, username, isAuthorised] # a list
 n[0] = -66 # we may mutate members of the list collection
 print(f'User {n[1]} is {n[0]}') # use slicing to acess ordinal members of a collection
 
+# it is a convention to use intitial capital letter for a class name
+class User():
+    '''this class will encapsulate user details
+    Name will be a non-empty string'''
+    def __init__(self, n):
+        '''the __init__ function is called every time we use the class'''
+        self.name = n
 
-class User:
-    pass
+if __name__ == '__main__':
+    # exercise the code
+    userA = User('Ethel') # we now have an instance of the class
+    print( userA.name )
